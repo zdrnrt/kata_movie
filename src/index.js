faf4ac3bce6b35e9aeec73ca6be74aec1ea397e8
Задорин Артем <artfan95@gmail.com>
2024-06-25 02:04:37 +0700

    Начальная настройка проекта

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+let title = <h1>react title</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(title);
