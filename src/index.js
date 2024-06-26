13394d14721bf9880103ca18472e896d147d509c
Задорин Артем <artfan95@gmail.com>
2024-06-26 23:23:53 +0700

    Добавлена тема Darkly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom"
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./css/bootstrap.min.css";
 let title = <h1>react title</h1>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,39 @@ const FilemanagerWebpackPlugin = require('filemanager-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.[contenthash].js',
-        path: path.resolve(__dirname, 'app/')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, 'src/index.html'),
-          filename: 'index.html',
-        }),
-        new FilemanagerWebpackPlugin({
-          events: {
-            onStart: {
-              delete: ['app'],
-            },
-          },
-        }),
-      ],
-      devServer: {
-        static: path.resolve(__dirname, 'app'),
-      }
-}
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.[contenthash].js',
+    path: path.resolve(__dirname, 'app/'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+      filename: 'index.html',
+    }),
+    new FilemanagerWebpackPlugin({
+      events: {
+        onStart: {
+          delete: ['app'],
+        },
+      },
+    }),
+  ],
+  devServer: {
+    static: path.resolve(__dirname, 'app'),
+  },
+};
